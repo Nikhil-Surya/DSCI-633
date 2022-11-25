43512312bfdbbf38b421f1a9a24791238db4317c
Nikhil <ns2330@rit.edu>
2022-11-25 03:19:43 -0500

    Project

--- assignments/project/project.py
+++ assignments/project/project.py
@@ -65,19 +65,4 @@ class my_model:
         return predictionsOfModel
 
 
-if __name__ == "__main__":
-    start = time.time()
-    # Load data
-    data = pd.read_csv("../data/job_train.csv")
-    # Replace missing values with empty strings
-    data = data.fillna("")
-    y = data["fraudulent"]
-    X = data.drop(['fraudulent'], axis=1)
-    # Train model
-    clf = my_model()
-    clf.fit(X, y)
-    runtime = (time.time() - start) / 60.0
-    print(runtime)
-    predictions = clf.predict(X)
-    print(predictions)
 
