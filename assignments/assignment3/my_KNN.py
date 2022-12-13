4124a760fbd8c373d38709bd35685fb4ab541b8e
Nikhil <ns2330@rit.edu>
2022-12-12 23:56:19 -0500

    resubmission

--- assignments/assignment3/my_KNN.py
+++ assignments/assignment3/my_KNN.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 from collections import Counter
-from math import *
-from decimal import Decimal
 
 class my_KNN:
 
@@ -19,76 +17,93 @@ class my_KNN:
         # X: pd.DataFrame, independent variables, float
         # y: list, np.array or pd.Series, dependent variables, int or str
         self.classes_ = list(set(list(y)))
-        # write your code below
         self.X = X
         self.y = y
-        return
-    def p_root(self, value, root):
 
-        return round(Decimal(value) **
-                     Decimal(1/float(root)), 3)
+    def dist(self,x):
+        # Calculate distances of training data to a single input data point (distances from self.X to x)
+        # Output np.array([distances to x])
 
-    # Find distance based on metric
-    def distance(self, x):
-        distances = list()
-        for train_row, classLabel in zip(self.X.to_numpy(), self.y):
-            distance = 0.0
-            z = zip(train_row, x)
-            # Pairing train_row and x using zip function.
-            if self.metric == "minkowski":
-                distance = self.p_root(sum(pow(abs(a - b), self.p) for a, b in z), self.p)
+        met = {"X": self.X, "p": self.p, "x": x}
+        algorithm = dict()
+        algorithm["euclidean"] = euclidean
+        algorithm["manhattan"] = manhattan
+        algorithm["cosine"] = cosine
+        algorithm["minkowski"] = minkowski
 
-            elif self.metric == "euclidean":
-                distance = sqrt(sum(pow(a - b, 2) for a, b in z))
+        if not self.metric in algorithm:
+            raise Exception("Unknown criterion.")
 
+        return algorithm[self.metric](**met)
 
-            elif self.metric == "manhattan":
-                distance = sum(abs(a - b) for a, b in z)
-
-
-            elif self.metric == "cosine":
-                distance = np.dot(train_row, x) / (np.sqrt(np.dot(train_row, x)) * np.sqrt(np.dot(train_row, x)))
-
-            distances.append((classLabel, distance))
-
-        return distances
-
-    # Locate the most similar neighbors
-    def k_neighbors(self, x):
+    def k_neighbors(self,x):
         # Return the stats of the labels of k nearest neighbors to a single input data point (np.array)
         # Output: Counter(labels of the self.n_neighbors nearest neighbors)
-        distances = self.distance(x)
-        distances.sort(key=lambda tup: tup[1], reverse=True)
-        #Arranging in decending order
-        neighbors = list()
-        for i in range(self.n_neighbors):
-            neighbors.append(distances[i][0])
+        distances = self.dist(x)
+        labels = self.y
+        mapped_labels = [(distance, label) for distance, label in zip(distances, labels)]
+        mapped_labels.sort(key=lambda x: x[0])
 
-        return Counter(neighbors)
+        output = Counter(map(lambda element: element[1], mapped_labels[:self.n_neighbors]))
+        return output
 
     def predict(self, X):
         # X: pd.DataFrame, independent variables, float
         # return predictions: list
-        # write your code below
         probs = self.predict_proba(X)
         predictions = [self.classes_[np.argmax(prob)] for prob in probs.to_numpy()]
-
         return predictions
 
     def predict_proba(self, X):
         # X: pd.DataFrame, independent variables, float
         # prob is a dict of prediction probabilities belonging to each categories
         # return probs = pd.DataFrame(list of prob, columns = self.classes_)
-        # write your code below
         probs = []
+        try:
+            X_feature = X[self.X.columns]
+        except:
+            raise Exception("Input data mismatch.")
 
-        for x in X[self.X.columns].to_numpy():
+        for x in X_feature.to_numpy():
             neighbors = self.k_neighbors(x)
             probs.append({key: neighbors[key] / float(self.n_neighbors) for key in self.classes_})
-
         probs = pd.DataFrame(probs, columns=self.classes_)
-
         return probs
 
+def minkowski(X, p, x):
+    #Implementation of the minkowski distance
+    distances = []
+    points = [row.tolist() for ind, row in X.iterrows()]
+    for point in points:
+        distances.append(sum([abs(point[index] - x[index]) ** p for index in range(len(point))]))
+    return np.array(distances)
 
 
+def euclidean(X, x,p):
+    #Implementation of the euclidean distance
+    distances = []
+    points = [row.tolist() for ind, row in X.iterrows()]
+    for point in points:
+        distances.append(np.sqrt(sum([((point[index] - x[index]) ** 2) for index in range(len(point))])))
+    return np.array(distances)
+
+def manhattan(X,x,p):
+    #Implementation of the manhattan distance
+    distances = []
+    points = [row.tolist() for ind, row in X.iterrows()]
+    for point in points:
+        distances.append(sum([abs(point[index] - x[index]) for index in range(len(point))]))
+    return np.array(distances)
+
+
+def cosine(X,x,p):
+    #Implementation of the cosine distance
+    distances = []
+    points = [row.tolist() for ind, row in X.iterrows()]
+    for point in points:
+        sumof_points = sum([point[x] ** 2 for x in range(len(point))])
+        sumof_x = sum([x[v] ** 2 for v in range(len(x))])
+        num = sum([point[val] * x[val] for val in range(len(point))])
+        denom = np.sqrt(sumof_points) * np.sqrt(sumof_x)
+        distances.append(1-float(num/denom))
+    return np.array(distances)
